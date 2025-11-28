d237e4ecb3def724241e85146dc6d859dd6422ed
Olhares - Tech <olhares.tecnologia@gmail.com>
2025-11-28 14:59:37 -0300

    Update Size WhatsAppButton.tsx

--- components/WhatsAppButton.tsx
+++ components/WhatsAppButton.tsx
@@ -15,7 +15,7 @@ export default function WhatsAppButton() {
       className="fixed bottom-6 right-6 bg-green-500 text-white p-4 rounded-full shadow-lg hover:bg-green-600 transition-all z-50 whatsapp-float"
       aria-label="Contato via WhatsApp"
     >
-      <MessageCircle size={32} />
+      <MessageCircle size={40} />
     </a>
   )
 }
